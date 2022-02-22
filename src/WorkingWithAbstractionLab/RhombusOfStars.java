f542187646f6b159de135e162f371e64d74b2e25
Milena <milenka.dimova@gmail.com>
2022-02-22 22:07:29 +0200

    first commit

---
+++ src/WorkingWithAbstractionLab/RhombusOfStars.java
@@ -0,0 +1,36 @@
+package WorkingWithAbstractionLab;
+
+import java.util.Scanner;
+
+public class RhombusOfStars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = Integer.parseInt(scanner.nextLine());
+
+        printTopHalf(size);
+        printBottomHalf(size);
+
+    }
+
+    private static void printBottomHalf(int size) {
+        for (int starCount = size - 1; starCount >  0; starCount--) {
+            printRow(size, starCount);
+        }
+    }
+
+    private static void printTopHalf(int size) {
+        for (int starCount = 1; starCount <= size; starCount++) {
+            printRow(size, starCount);
+        }
+    }
+
+    private static void printRow(int size, int starCount) {
+        for (int i = 0; i < size - starCount; i++) {
+            System.out.print(" ");
+        }
+        for (int i = 0; i < starCount - 1; i++) {
+            System.out.print("* ");
+        }
+        System.out.println("*");
+    }
+}
