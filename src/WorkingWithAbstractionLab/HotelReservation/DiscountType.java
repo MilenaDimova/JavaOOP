e65190a54489ddee2a5d4d8cdae182693a29240e
Milena <milenka.dimova@gmail.com>
2022-02-23 23:41:13 +0200

    add new task

---
+++ src/WorkingWithAbstractionLab/HotelReservation/DiscountType.java
@@ -0,0 +1,18 @@
+package WorkingWithAbstractionLab.HotelReservation;
+
+public enum DiscountType {
+    None(0),
+    SecondVisit(10),
+    VIP(20);
+
+    private int discount;
+
+    DiscountType(int discount) {
+        this.discount = discount;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+}
