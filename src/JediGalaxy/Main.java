f18476017970461352ae130ac08240adb58f7ef9
Milena <milenka.dimova@gmail.com>
2022-02-25 22:43:33 +0200

    add new task

---
+++ src/JediGalaxy/Main.java
@@ -0,0 +1,64 @@
+package JediGalaxy;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] dimensions = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        int rows = dimensions[0];
+        int cols = dimensions[1];
+
+        int[][] matrix = new int[rows][cols];
+        int value = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = value++;
+            }
+        }
+
+        String command = scanner.nextLine();
+        long sum = 0;
+        while (!command.equals("Let the Force be with you")) {
+            int[] heroCoordinates = Arrays.stream(command.split("\\s+"))
+                    .mapToInt(Integer::parseInt).toArray();
+            int[] evilCoordinates = Arrays.stream(scanner.nextLine().split("\\s+"))
+                    .mapToInt(Integer::parseInt).toArray();
+            int evilRow = evilCoordinates[0];
+            int evilCol = evilCoordinates[1];
+            moveEvil(matrix, evilRow, evilCol);
+
+            int heroRow = heroCoordinates[0];
+            int heroCol = heroCoordinates[1];
+            sum = getHeroSumValues(matrix, sum, heroRow, heroCol);
+
+            command = scanner.nextLine();
+        }
+
+        System.out.println(sum);
+
+    }
+
+    private static long getHeroSumValues(int[][] matrix, long sum, int heroRow, int heroCol) {
+        while (heroRow >= 0 && heroCol < matrix[1].length) {
+            if (heroRow < matrix.length && heroCol >= 0 && heroCol < matrix[0].length) {
+                sum += matrix[heroRow][heroCol];
+            }
+            heroCol++;
+            heroRow--;
+        }
+        return sum;
+    }
+
+    private static void moveEvil(int[][] matrix, int row, int col) {
+        while (row >= 0 && col >= 0) {
+            if (row < matrix.length && col < matrix[0].length) {
+                matrix[row][col] = 0;
+            }
+            row--;
+            col--;
+        }
+    }
+}
