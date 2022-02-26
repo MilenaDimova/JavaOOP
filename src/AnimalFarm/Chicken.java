896e6ea165426d61e482e658c9df9ea205d371d6
Milena <milenka.dimova@gmail.com>
2022-02-26 17:48:02 +0200

    add new task

---
+++ src/AnimalFarm/Chicken.java
@@ -0,0 +1,48 @@
+package AnimalFarm;
+
+public class Chicken {
+    private String name;
+    private int age;
+
+    public Chicken(String name, int age) {
+        this.setName(name);
+        this.setAge(age);
+    }
+
+    private void setName(String name) {
+        if (!name.trim().isEmpty()) {
+            this.name = name;
+        } else {
+            throw new IllegalArgumentException("Name cannot be empty.");
+        }
+    }
+
+    public void setAge(int age) {
+        if (age >= 0 && age <= 15) {
+            this.age = age;
+        } else {
+            throw new IllegalArgumentException("Age should be between 0 and 15.");
+        }
+    }
+
+    public double productPerDay() {
+        return calculateProductPerDay();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Chicken %s (age %d) can produce %.2f eggs per day.", this.name, this.age, this.productPerDay());
+    }
+
+    private double calculateProductPerDay() {
+        double eggsPerDay = 0;
+        if (this.age <= 5) {
+            eggsPerDay = 2;
+        } else if (this.age <= 11) {
+            eggsPerDay = 1;
+        } else if (this.age <= 15) {
+            eggsPerDay = 0.75;
+        }
+        return eggsPerDay;
+    }
+}
